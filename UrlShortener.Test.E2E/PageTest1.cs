7efd9198d76bd76181c25fc6412f40e6d0efba74
SimonLiebers-Dev <48295840+SimonLiebers-Dev@users.noreply.github.com>
2025-03-27 16:24:14 +0100

    add e2e projects

---
+++ UrlShortener.Test.E2E/PageTest1.cs
@@ -0,0 +1,25 @@
+﻿using UrlShortener.Test.E2E.Utils;
+
+namespace UrlShortener.Test.E2E
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class PageTest1
+    {
+        [Test]
+        public async Task Test()
+        {
+            var test = await PlaywrightUtils.GetPlaywrightTestAsync(false);
+
+            await test.GotoPageAsync(string.Empty, async page =>
+            {
+                var body = page.Locator("body");
+                await body.WaitForAsync();
+
+                var title = await page.TitleAsync();
+
+                Assert.That(title, Is.EqualTo("Login"));
+            });
+        }
+    }
+}
