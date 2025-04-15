6c89e78b517a7afdcd21f1f49d2912606bb5ea95
SimonLiebers-Dev <48295840+SimonLiebers-Dev@users.noreply.github.com>
2025-04-15 20:10:15 +0200

    inc version for rtd docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'UrlShortener'
 copyright = '2021, Liebers'
 author = 'Simon Liebers'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.2'
+version = '0.2.0'
 
 # -- General configuration
 
