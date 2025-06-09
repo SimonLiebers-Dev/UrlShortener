8faec972ee01c3ae85053566623f5b1e021267d5
@studliebsi5438 <simon.liebers@stud.th-rosenheim.de>
2025-06-09 21:16:32 +0200

    fix issues

--- UrlShortener.Test.Shared/Unit/DtoExtensionTests.cs
+++ UrlShortener.Test.Shared/Unit/DtoExtensionTests.cs
@@ -39,7 +39,7 @@ namespace UrlShortener.Test.Shared.Unit
                 Assert.That(dto.ShortUrl, Is.EqualTo("https://localhost:5001/abc123"));
                 Assert.That(dto.CreatedAt, Is.EqualTo(mapping.CreatedAt));
                 Assert.That(dto.User, Is.EqualTo(mapping.User));
-                Assert.That(dto.RedirectLogs.Count, Is.EqualTo(1));
+                Assert.That(dto.RedirectLogs, Has.Count.EqualTo(1));
             });
         }
 
@@ -66,20 +66,23 @@ namespace UrlShortener.Test.Shared.Unit
 
             var dto = log.ToDto();
 
-            Assert.That(dto.Id, Is.EqualTo(log.Id));
-            Assert.That(dto.IpAddress, Is.EqualTo(log.IpAddress));
-            Assert.That(dto.UserAgent, Is.EqualTo(log.UserAgent));
-            Assert.That(dto.AccessedAt, Is.EqualTo(log.AccessedAt));
-            Assert.That(dto.BrowserFamily, Is.EqualTo(log.BrowserFamily));
-            Assert.That(dto.ClientEngine, Is.EqualTo(log.ClientEngine));
-            Assert.That(dto.ClientName, Is.EqualTo(log.ClientName));
-            Assert.That(dto.ClientType, Is.EqualTo(log.ClientType));
-            Assert.That(dto.DeviceBrand, Is.EqualTo(log.DeviceBrand));
-            Assert.That(dto.DeviceModel, Is.EqualTo(log.DeviceModel));
-            Assert.That(dto.DeviceType, Is.EqualTo(log.DeviceType));
-            Assert.That(dto.OsName, Is.EqualTo(log.OsName));
-            Assert.That(dto.OsVersion, Is.EqualTo(log.OsVersion));
-            Assert.That(dto.OsFamily, Is.EqualTo(log.OsFamily));
+            Assert.Multiple(() =>
+            {
+                Assert.That(dto.Id, Is.EqualTo(log.Id));
+                Assert.That(dto.IpAddress, Is.EqualTo(log.IpAddress));
+                Assert.That(dto.UserAgent, Is.EqualTo(log.UserAgent));
+                Assert.That(dto.AccessedAt, Is.EqualTo(log.AccessedAt));
+                Assert.That(dto.BrowserFamily, Is.EqualTo(log.BrowserFamily));
+                Assert.That(dto.ClientEngine, Is.EqualTo(log.ClientEngine));
+                Assert.That(dto.ClientName, Is.EqualTo(log.ClientName));
+                Assert.That(dto.ClientType, Is.EqualTo(log.ClientType));
+                Assert.That(dto.DeviceBrand, Is.EqualTo(log.DeviceBrand));
+                Assert.That(dto.DeviceModel, Is.EqualTo(log.DeviceModel));
+                Assert.That(dto.DeviceType, Is.EqualTo(log.DeviceType));
+                Assert.That(dto.OsName, Is.EqualTo(log.OsName));
+                Assert.That(dto.OsVersion, Is.EqualTo(log.OsVersion));
+                Assert.That(dto.OsFamily, Is.EqualTo(log.OsFamily));
+            });
         }
 
         [Test]
@@ -99,9 +102,12 @@ namespace UrlShortener.Test.Shared.Unit
 
             var stats = mappings.GetStats();
 
-            Assert.That(stats.Clicks, Is.EqualTo(3));
-            Assert.That(stats.Mappings, Is.EqualTo(1));
-            Assert.That(stats.DeviceTypeStats.Count, Is.EqualTo(2));
+            Assert.Multiple(() =>
+            {
+                Assert.That(stats.Clicks, Is.EqualTo(3));
+                Assert.That(stats.Mappings, Is.EqualTo(1));
+                Assert.That(stats.DeviceTypeStats.Count, Is.EqualTo(2));
+            });
             Assert.That(stats.DeviceTypeStats.FirstOrDefault(d => d.DeviceType == "Mobile")?.Clicks, Is.EqualTo(2));
             Assert.That(stats.TimeSeriesStats.Count, Is.EqualTo(1));
             Assert.That(stats.TimeSeriesStats.First().ClicksPerDay.Sum(c => c.Clicks), Is.EqualTo(3));
